0d36b287295395c0e2f41808b04f7bec405910d0
athira <athiraos1997@gmail.com>
2023-07-22 01:40:22 +0530

    added mongoose ,added assets with static files

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
 
+//to get static files assets
+app.use(express.static('./assets'));
+app.use(expressLayouts);
+//extract style and scripts from sub pages into layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 //use express router
 app.use('/',require('./routes'));
+
+
+//setting up view enjine
+app.set('view engine','ejs');
+app.set('views','./views')
+
 
 
 
