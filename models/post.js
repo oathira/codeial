4abc972cd6d32d22a52bc3ee0df5640e26c0e660
athira <athiraos1997@gmail.com>
2023-08-11 06:05:58 +0530

    adding comments to db

--- models/post.js
+++ models/post.js
@@ -1,14 +1,19 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema({
-    content:{
+    content : {
         type:String,
         required:true
     },
-    user:{
+    user : {
         type:mongoose.Schema.Types.ObjectId,
         ref : 'User'
-    }
+    },
+    //include the array of ids of all comments in this post schema itself
+    comments : [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'comment'
+    }]
 },{
     timestamps:true
 });
